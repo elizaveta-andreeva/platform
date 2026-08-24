2ac90d8aea3c117b9de28608a954efeeacc25b90
Rhoda Eboreime <rhodaivioje@gmail.com>
2026-08-24 14:04:18 +0200

    Align Energy IVML and Lua simulator with concrete values

--- platform/documentation/devel/examples/custom.energy.script.lua
+++ platform/documentation/devel/examples/custom.energy.script.lua
@@ -1,16 +1,23 @@
 -- custom.energy.script.lua
--- Initial Lua-based OPC UA Energy Aggregator prototype
--- Based on the existing oktoflow custom.script.lua example.
+-- Energy simulator for generated Energy connector validation.
+-- Exposes the concrete Energy measurement values expected by energy.ivml.
 
 -- Helper variables
 last_update = os.time()
 wait_time = 1 -- seconds
 
 -- Initial simulated Energy values
-nonElectricalEnergyValue = 100.0
-baseFlowValue = 20.0
-volumeFlowValue = 10.0
-massFlowValue = 5.0
+neEnergyImportHpValue = 100.0
+neEnergyExportHpValue = 20.0
+
+pressureValue = 5.0
+temperatureValue = 22.0
+
+volumeValue = 10.0
+volumeFlowRateValue = 2.0
+
+massValue = 15.0
+massFlowRateValue = 3.0
 
 -- Helper functions
 function get_elapsed()
@@ -28,119 +35,216 @@ function should_update()
   return true
 end
 
--- Create a namespace
+-- Create namespace
 ns = Server.addNamespace("Static")
 
--- Create value variants
-NonElectricalEnergy_variant = Variant.new(DataType.DOUBLE)
-NonElectricalEnergy_variant:setScalar(nonElectricalEnergyValue)
+-- Create OPC UA value variants
 
-BaseFlow_variant = Variant.new(DataType.DOUBLE)
-BaseFlow_variant:setScalar(baseFlowValue)
+-- NonElectricalEnergy values are DoubleType in OpcEnergy.ivml
+NeEnergyImportHp_variant = Variant.new(DataType.DOUBLE)
+NeEnergyImportHp_variant:setScalar(neEnergyImportHpValue)
 
-VolumeFlow_variant = Variant.new(DataType.DOUBLE)
-VolumeFlow_variant:setScalar(volumeFlowValue)
+NeEnergyExportHp_variant = Variant.new(DataType.DOUBLE)
+NeEnergyExportHp_variant:setScalar(neEnergyExportHpValue)
 
-MassFlow_variant = Variant.new(DataType.DOUBLE)
-MassFlow_variant:setScalar(massFlowValue)
+-- Remaining Energy measurements are FloatType in OpcEnergy.ivml
+Pressure_variant = Variant.new(DataType.FLOAT)
+Pressure_variant:setScalar(pressureValue)
+
+Temperature_variant = Variant.new(DataType.FLOAT)
+Temperature_variant:setScalar(temperatureValue)
+
+Volume_variant = Variant.new(DataType.FLOAT)
+Volume_variant:setScalar(volumeValue)
+
+VolumeFlowRate_variant = Variant.new(DataType.FLOAT)
+VolumeFlowRate_variant:setScalar(volumeFlowRateValue)
+
+Mass_variant = Variant.new(DataType.FLOAT)
+Mass_variant:setScalar(massValue)
+
+MassFlowRate_variant = Variant.new(DataType.FLOAT)
+MassFlowRate_variant:setScalar(massFlowRateValue)
+
 
 function add_Energy_nodes()
 
-  -- Create namespace for the reused Sinumerik root
+  -- Reuse Sinumerik as the root used by the validation setup
   ns2 = Server.addNamespace("Sinumerik")
 
-  -- Add Sinumerik folder to the Objects folder
+  -- Objects/Sinumerik
   Sinumerik_folder = ObjectNode.newRootFolder("Sinumerik", ns2)
   Server.addObjectNode(Sinumerik_folder)
 
-  -- Add Energy folder to the Sinumerik folder
-  Energy_folder = Sinumerik_folder.newFolder("Energy", ns2, Sinumerik_folder:getNodeId())
+  -- Objects/Sinumerik/Energy
+  Energy_folder = Sinumerik_folder.newFolder(
+    "Energy",
+    ns2,
+    Sinumerik_folder:getNodeId()
+  )
   Sinumerik_folder.addObjectNode(Energy_folder)
 
-  -- Add NonElectricalEnergy variable to the Energy folder
-  NonElectricalEnergy = VariableNode.new(
-    NodeId.newString("NonElectricalEnergy", ns2),
-    "NonElectricalEnergy",
+  -- Objects/Sinumerik/Energy/NeEnergyImportHp
+  NeEnergyImportHp = VariableNode.new(
+    NodeId.newString("NeEnergyImportHp", ns2),
+    "NeEnergyImportHp",
     Energy_folder:getNodeId(),
-    NonElectricalEnergy_variant,
+    NeEnergyImportHp_variant,
     AccessLevel.READ
   )
-  Server.addVariableNode(NonElectricalEnergy)
+  Server.addVariableNode(NeEnergyImportHp)
 
-  -- Add BaseFlow variable to the Energy folder
-  BaseFlow = VariableNode.new(
-    NodeId.newString("BaseFlow", ns2),
-    "BaseFlow",
+  -- Objects/Sinumerik/Energy/NeEnergyExportHp
+  NeEnergyExportHp = VariableNode.new(
+    NodeId.newString("NeEnergyExportHp", ns2),
+    "NeEnergyExportHp",
     Energy_folder:getNodeId(),
-    BaseFlow_variant,
+    NeEnergyExportHp_variant,
     AccessLevel.READ
   )
-  Server.addVariableNode(BaseFlow)
+  Server.addVariableNode(NeEnergyExportHp)
 
-  -- Add VolumeFlow variable to the Energy folder
-  VolumeFlow = VariableNode.new(
-    NodeId.newString("VolumeFlow", ns2),
-    "VolumeFlow",
+  -- Objects/Sinumerik/Energy/Pressure
+  Pressure = VariableNode.new(
+    NodeId.newString("Pressure", ns2),
+    "Pressure",
     Energy_folder:getNodeId(),
-    VolumeFlow_variant,
+    Pressure_variant,
     AccessLevel.READ
   )
-  Server.addVariableNode(VolumeFlow)
+  Server.addVariableNode(Pressure)
 
-  -- Add MassFlow variable to the Energy folder
-  MassFlow = VariableNode.new(
-    NodeId.newString("MassFlow", ns2),
-    "MassFlow",
+  -- Objects/Sinumerik/Energy/Temperature
+  Temperature = VariableNode.new(
+    NodeId.newString("Temperature", ns2),
+    "Temperature",
     Energy_folder:getNodeId(),
-    MassFlow_variant,
+    Temperature_variant,
     AccessLevel.READ
   )
-  Server.addVariableNode(MassFlow)
+  Server.addVariableNode(Temperature)
+
+  -- Objects/Sinumerik/Energy/Volume
+  Volume = VariableNode.new(
+    NodeId.newString("Volume", ns2),
+    "Volume",
+    Energy_folder:getNodeId(),
+    Volume_variant,
+    AccessLevel.READ
+  )
+  Server.addVariableNode(Volume)
+
+  -- Objects/Sinumerik/Energy/VolumeFlowRate
+  VolumeFlowRate = VariableNode.new(
+    NodeId.newString("VolumeFlowRate", ns2),
+    "VolumeFlowRate",
+    Energy_folder:getNodeId(),
+    VolumeFlowRate_variant,
+    AccessLevel.READ
+  )
+  Server.addVariableNode(VolumeFlowRate)
+
+  -- Objects/Sinumerik/Energy/Mass
+  Mass = VariableNode.new(
+    NodeId.newString("Mass", ns2),
+    "Mass",
+    Energy_folder:getNodeId(),
+    Mass_variant,
+    AccessLevel.READ
+  )
+  Server.addVariableNode(Mass)
+
+  -- Objects/Sinumerik/Energy/MassFlowRate
+  MassFlowRate = VariableNode.new(
+    NodeId.newString("MassFlowRate", ns2),
+    "MassFlowRate",
+    Energy_folder:getNodeId(),
+    MassFlowRate_variant,
+    AccessLevel.READ
+  )
+  Server.addVariableNode(MassFlowRate)
 
 end
 
 add_Energy_nodes()
 
--- Update node values every 1 second
+
+-- Update simulated values every second
 function Update()
+
   if not should_update() then
     return
   end
 
-  -- Simple simulated value changes for initial prototype only
-  nonElectricalEnergyValue = nonElectricalEnergyValue + 1.0
-  baseFlowValue = baseFlowValue + 0.5
-  volumeFlowValue = volumeFlowValue + 0.2
-  massFlowValue = massFlowValue + 0.1
+  neEnergyImportHpValue = neEnergyImportHpValue + 1.0
+  neEnergyExportHpValue = neEnergyExportHpValue + 0.5
 
-  -- Reset values to keep them within a simple test range
-  if nonElectricalEnergyValue > 150.0 then
-    nonElectricalEnergyValue = 100.0
+  pressureValue = pressureValue + 0.1
+  temperatureValue = temperatureValue + 0.1
+
+  volumeValue = volumeValue + 0.2
+  volumeFlowRateValue = volumeFlowRateValue + 0.1
+
+  massValue = massValue + 0.2
+  massFlowRateValue = massFlowRateValue + 0.1
+
+  -- Keep values within simple validation ranges
+  if neEnergyImportHpValue > 150.0 then
+    neEnergyImportHpValue = 100.0
   end
 
-  if baseFlowValue > 30.0 then
-    baseFlowValue = 20.0
+  if neEnergyExportHpValue > 40.0 then
+    neEnergyExportHpValue = 20.0
   end
 
-  if volumeFlowValue > 15.0 then
-    volumeFlowValue = 10.0
+  if pressureValue > 10.0 then
+    pressureValue = 5.0
   end
 
-  if massFlowValue > 8.0 then
-    massFlowValue = 5.0
+  if temperatureValue > 30.0 then
+    temperatureValue = 22.0
   end
 
-  -- Update OPC UA variable values
-  NonElectricalEnergy_variant:setScalar(nonElectricalEnergyValue)
-  BaseFlow_variant:setScalar(baseFlowValue)
-  VolumeFlow_variant:setScalar(volumeFlowValue)
-  MassFlow_variant:setScalar(massFlowValue)
+  if volumeValue > 20.0 then
+    volumeValue = 10.0
+  end
 
-  NonElectricalEnergy:updateValue()
-  BaseFlow:updateValue()
-  VolumeFlow:updateValue()
-  MassFlow:updateValue()
+  if volumeFlowRateValue > 5.0 then
+    volumeFlowRateValue = 2.0
+  end
+
+  if massValue > 25.0 then
+    massValue = 15.0
+  end
+
+  if massFlowRateValue > 6.0 then
+    massFlowRateValue = 3.0
+  end
+
+  -- Update OPC UA values
+  NeEnergyImportHp_variant:setScalar(neEnergyImportHpValue)
+  NeEnergyExportHp_variant:setScalar(neEnergyExportHpValue)
+
+  Pressure_variant:setScalar(pressureValue)
+  Temperature_variant:setScalar(temperatureValue)
+
+  Volume_variant:setScalar(volumeValue)
+  VolumeFlowRate_variant:setScalar(volumeFlowRateValue)
+
+  Mass_variant:setScalar(massValue)
+  MassFlowRate_variant:setScalar(massFlowRateValue)
+
+  NeEnergyImportHp:updateValue()
+  NeEnergyExportHp:updateValue()
+
+  Pressure:updateValue()
+  Temperature:updateValue()
+
+  Volume:updateValue()
+  VolumeFlowRate:updateValue()
+
+  Mass:updateValue()
+  MassFlowRate:updateValue()
 
 end
-
 
